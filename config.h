3280b3c21a026c223ec09c6b7705c272df75b6f3
Julio Arias <jarias@users.noreply.github.com>
2020-07-10 09:14:07 -0600

    feat: make zoom main window floating

--- config.h
+++ config.h
@@ -32,6 +32,7 @@ static const Rule rules[] = {
 	{ "Gimp",     NULL,       NULL,                       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,                       1 << 8,       0,           -1 },
 	{ "firefox",  "Toolkit",  "Picture-in-Picture",       TAGMASK,      1,           -1 },
+	{ "zoom",     "zoom",     "Zoom - Free Account",      TAGMASK,      1,           -1 },
 };
 
 /* layout(s) */
