f1e0637454d581694b9ad52d711167bfcc2cdcef
Julio Arias <jarias@users.noreply.github.com>
2020-08-04 20:53:40 -0600

    fix window rules

--- PKGBUILD
+++ PKGBUILD
@@ -3,7 +3,7 @@
 
 pkgname=dwm
 pkgver=6.2
-pkgrel=4
+pkgrel=5
 pkgdesc="A dynamic window manager for X"
 url="http://dwm.suckless.org"
 arch=('i686' 'x86_64')
@@ -12,7 +12,7 @@ options=(zipman)
 depends=('libx11' 'libxinerama' 'libxft' 'freetype2')
 source=(http://dl.suckless.org/dwm/dwm-$pkgver.tar.gz config.h)
 sha256sums=('97902e2e007aaeaa3c6e3bed1f81785b817b7413947f1db1d3b62b8da4cd110e'
-            '2ee88278b76c3dbf836c2c71f0f88c9fafc8e945139c896daf7913c7f43aeac3')
+            '09790d26cc000688097e8e8e622a7469e2c4b0792d6dbff74b138de5b23c20e9')
 
 prepare() {
   cd "$srcdir/$pkgname-$pkgver"
--- config.h
+++ config.h
@@ -30,9 +30,10 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title                       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,                       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,                       1 << 8,       0,           -1 },
+	{ "Firefox",  NULL,       NULL,                       0,            0,           -1 },
 	{ "firefox",  "Toolkit",  "Picture-in-Picture",       TAGMASK,      1,           -1 },
-	{ "zoom",     "zoom",     "Zoom - Free Account",      TAGMASK,      1,           -1 },
+	{ "zoom",     "zoom",     "Zoom - Free Account",      0,            1,           -1 },
+	{ "Dunst",    "Dunst"     "Dunst",                    0,            1,           -1 },
 };
 
 /* layout(s) */
